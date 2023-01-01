17727e192bb50d76a77fdbdc482b2dea0b1056a2
chetex <chetex69@gmail.com>
2023-01-01 18:09:14 +0100

    2 new projects

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Starting point of all app
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Center(
+        child: Text("Hello center"),
+      ),
+    ),
+  );
+}
