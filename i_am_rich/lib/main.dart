039fe473a2cbfa9e79e00e2ee2977d3a38a93ec3
chetex <chetex69@gmail.com>
2023-01-02 10:24:05 +0100

    Change to rich app

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
 
-/// Starting point of all app
-void main() {
-  runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("Hello center"),
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: MyStatefulWidget(),
+    );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({super.key});
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('I am rich'),
+        backgroundColor: Colors.blueGrey[900],
       ),
-    ),
-  );
+      backgroundColor: Colors.grey,
+      body: const Center(
+        child: Image(
+          image: NetworkImage(
+            "https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg",
+          ),
+        ),
+      ),
+    );
+  }
 }
