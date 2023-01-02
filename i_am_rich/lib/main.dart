d858f489ad0f867dc3b7a5d55f2eb7ced57448c7
chetex <chetex69@gmail.com>
2023-01-02 12:15:04 +0100

    Change to rich app. Add new android icon.

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -34,9 +34,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       backgroundColor: Colors.grey,
       body: const Center(
         child: Image(
-          image: NetworkImage(
-            "https://images.ctfassets.net/hrltx12pl8hq/3j5RylRv1ZdswxcBaMi0y7/b84fa97296bd2350db6ea194c0dce7db/Music_Icon.jpg",
-          ),
+          image: AssetImage('assets/images/diamond.png'),
         ),
       ),
     );
